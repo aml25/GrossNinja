f41408df91cfd20d4e5cd3db990ac4fcdb42a455
Adam.Laskowitz <lasko25@gmail.com>
2015-08-23 16:24:13 -0700

    Changed folder structure, temporary website placeholder is "Wall" which is a community wall to post messages.

---
+++ Music/public/js/home.js
@@ -0,0 +1,109 @@
+var columnCount = 3;
+var marginPercentage = 2;
+
+var dragging = false;
+
+function initIcons(){
+	$(".icons").each(function(i){
+		console.log(i);
+		$(this).height($(this).width());
+		$(this).animate({
+			"left": ((i%columnCount) * $("body").width()/columnCount) + "px",
+			"top": (Math.floor(i/columnCount) * ($(this).height() + $("body").height() * (marginPercentage/100))) + "px"
+		}, 500);
+
+		$(this).attr("data-count", i);
+
+		console.log($(this).css("top"));
+	});
+
+
+	$(".icons").draggable({
+		start: function(e){
+			dragging = true;
+			console.log(e);
+
+			$("#dragBox").css({
+				"background": "#e0e0e0"
+			});
+		},
+		drag: function(e){
+			if(mouseInsideDragBox(e.clientX, e.clientY)){
+				$("#dragBox").css({
+					"background": "#a1a1a1"
+				});
+				$("#dragBox").stop().animate({
+					"borderRadius": "10px",
+					"opacity": "0.25"
+				},500,"easeOutExpo");
+
+				var currWidth = $(this).width();
+			}
+			else{
+				$("#dragBox").css({
+					"background": "#e0e0e0"
+				});
+				$("#dragBox").stop().animate({
+					"borderRadius": map(distance(e.clientX,e.clientY,$(document).width()/2, $(document).height()), 0, $(document).height(), 20, 90),
+					"opacity": "1"
+				},500,"easeOutExpo");
+			}
+		},
+		stop: function(e){
+			dragging = false;
+
+			if(mouseInsideDragBox(e.clientX, e.clientY)){
+				$(this).stop().animate({
+					width: $(document).height()*1.5,
+					height: "150%",
+					borderRadius: "50%",
+					left: ($(document).width()/2) - ($(document).height()*1.5)/2,
+					top: ($(document).height()/2) - ($(document).height()*1.5)/2
+				}, {
+					duration: 1000,
+					queue: false,
+					easing: "easeInOutExpo"
+				});
+
+				var selected = this;
+				$('body').children().fadeOut({
+					duration: 500,
+					easing: "easeInOutCubic"
+				}); //hide all nodes directly under the body
+				$(this).appendTo('body');			
+			}
+			else{
+				$(this).animate({
+					"left": (($(this).attr("data-count")%columnCount) * $("body").width()/columnCount) + "px",
+					"top": (Math.floor($(this).attr("data-count")/columnCount) * ($(this).height() + $("body").height() * (marginPercentage/100))) + "px"
+				}, 500);;
+
+				$("#dragBox").css({
+					"background": "rgba(255,255,255,0)"
+				});
+				$("#dragBox").stop().animate({
+					"borderRadius": "90px",
+					"opacity": "1"
+				},500, "easeOutExpo");
+			}
+		}
+	});
+}
+
+function mouseInsideDragBox(mouseX, mouseY){
+	var dragBoxX = $("#dragBox").offset().left;
+	var dragBoxY = $("#dragBox").offset().top;
+	var dragBoxWidth = $("#dragBox").width();
+	var dragBoxHeight = $("#dragBox").height();
+	return mouseX >= dragBoxX && mouseX <= dragBoxX + dragBoxWidth && mouseY >= dragBoxY && mouseY <= dragBoxY + dragBoxHeight;
+}
+
+function distance(xa,ya,xb,yb){
+	return Math.sqrt(Math.pow(xa-xb, 2) + Math.pow(ya-yb,2));
+}
+
+function map(val,amin,amax,bmin,bmax){
+	return (val-amin)/(amax-amin) * (bmax-bmin) + bmin;
+}
+
+initIcons();
---
+++ Wall/server.js
@@ -0,0 +1,84 @@
+/*login-server.js*/
+
+/*** ALL THE REQUIRES ***/
+var fs = require('fs');
+var http = require('http');
+var express = require('express');
+var app = module.exports.app = express();
+var server = http.createServer(app); //the server, serving the files to clients
+var bodyParser = require('body-parser');
+var io = require('socket.io')(server);
+/*** ============================== ***/
+
+//config for username and password
+var messages = JSON.parse(fs.readFileSync("wall.json"));
+
+//set up headers for the server (not sure if this is really needed)
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
+//extend Express for JSON
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+//serve a folder called "public," which contains all the data to be served to clients
+app.use(express.static(__dirname + '/public'));
+
+/***do stuff here***/
+
+io.on('connection', function (socket) {
+
+	socket.on('newMessage', function (data) {
+		
+		data.id = Math.round((Math.random() * Math.random() * Math.random()) * 100000000); //set some unique id
+
+		console.log(data);
+		messages.messages.push(data);
+
+		fs.writeFile("wall.json", JSON.stringify(messages, null, '\t')); //save the users data, there's definitely a better way to do this
+		console.log("just wrote to the file");
+
+		io.sockets.emit('createdMessage', data);
+	});
+});
+
+app.post("/getMessages", function(req, res){
+	res.send(messages);
+});
+
+/*app.post("/newMessage", function(req, res){
+	//console.log(req.body);
+	messages.messages.push(req.body);
+
+	//console.log(messages);
+
+	fs.writeFile("wall.json", JSON.stringify(messages, null, '\t')); //save the users data, there's definitely a better way to do this
+	res.send(req.body);
+});*/
+
+//every second loop through messages and start to kill them
+setInterval(function(){
+	var deadMessages = [];
+	for(var i=0;i<messages.messages.length;i++){
+		messages.messages[i].timetodie -= 1000;
+		if(messages.messages[i].timetodie <= 0){
+			deadMessages.push(messages.messages[i].id);
+			messages.messages.splice(i,1);
+		}
+	}
+	if(deadMessages.length > 0){
+		console.log("removing a message and saving the file");
+		fs.writeFile("wall.json", JSON.stringify(messages, null, '\t')); //save the users data, there's definitely a better way to do this
+		io.sockets.emit('removeMessages', deadMessages);
+	}
+}, 1000);
+
+/*******************/
+
+/*** FINALLY, START LISTENING ON THE SERVER ***/
+server.listen(8999,  function(){
+    console.log(server.address());
+});
